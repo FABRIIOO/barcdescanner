1716fce80e21583e7b0269424c59c98852ebca9f
Fabricio Carneiro Piscinelli <36889604+FABRIIOO@users.noreply.github.com>
2018-07-29 19:55:15 -0300

    Initial commit

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-barcodescanner/www/barcodescanner.js",
+        "id": "cordova-plugin-barcodescanner.BarcodeScanner",
+        "pluginId": "cordova-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-barcodescanner": "0.7.4",
+    "cordova-plugin-compat": "1.0.0"
+}
+// BOTTOM OF METADATA
+});
